02e2ff793ffc10bfe455279013bd518b903e9911
evsecode <mynameisevse@gmail.com>
2024-03-12 20:57:26 +0300

    GitHub Pages fix.

---
+++ src/app/components/product/product.component.ts
@@ -0,0 +1,14 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {IProduct} from "../../models/product";
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.scss']
+})
+export class ProductComponent{
+
+  @Input() product: IProduct
+
+  details = false
+}
